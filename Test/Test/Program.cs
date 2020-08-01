3f766a3a72481bdc7363bfeae5f172d398c60872
Armandot1998 <armandotcajilema@gmailcom>
2020-08-01 13:34:26 -0500

    Bubble sort
    algoritmo de ordenamiento burbuja

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Test
 {
@@ -6,7 +7,28 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] entrada = {3, 3, 5, 2, 1, 6 };
+            int rango = entrada.Length -1; 
+
+            for (int a = 0; a < entrada.Length; a++)
+            {
+                for (int b = 0; b < entrada.Length - 1; b++)
+                {
+                    if (entrada[b] > entrada[b + 1])
+                    {
+                        int tmp = entrada[b];
+                        entrada[b] = entrada[b + 1];
+                        entrada[b + 1] = tmp;
+                    }
+                }
+            }
+
+            Console.WriteLine(entrada[0]);
+            Console.WriteLine(entrada[1]);
+            Console.WriteLine(entrada[2]);
+            Console.WriteLine(entrada[3]);
+            Console.WriteLine(entrada[4]);
+            Console.WriteLine(entrada[5]);
         }
     }
 }
